93c23e3ae83bf543d27bba72209dea4a0e77011c
Anna Rafaella Braga <arafaella24@gmail.com>
2022-07-12 10:45:16 -0300

    Projeto Site de Vagas

---
+++ Site-Vagas-MulheresEmTech/README.md
@@ -0,0 +1,30 @@
+## O projeto
+
+### Estrutura de arquivos
+
+- README.md
+- index.html
+- /assets
+  - /img
+  - /css
+    - style.css
+  - /js
+    - script.js
+
+### Fonte
+
+- 'Roboto', sans-serif;
+
+### Cores
+
+- Branco: #ffffff;
+- Cinza Claro: #cccccc;
+- Cinza Mais Claro 1: #f8f8f8;
+- Cinza Mais Claro 2: #d9d9d9;
+- Cinza Mais Claro 3: #fafafa;
+- Cinza Escuro 1: rgb(49, 49, 49);
+- Cinza Escuro 2: #606060;z
+
+## Layout
+
+- [Layout Mulheres em Tech => Desenvolvendo plataforma de vagas](https://www.figma.com/file/ivCfSG5ElZUqz5KLv2fmhn/Evento----Mulheres-em-Tech?node-id=39%3A135)
---
+++ Site-Vagas-MulheresEmTech/assets/js/vagas.js
@@ -0,0 +1,34 @@
+const vagas = [
+    {
+        id: 1,
+        nome: "Pessoa Desenvolvedora Front-End - React JS",
+        empresa: "Kenzie Academy",
+        localEmpresa: "Curitiba",
+        descricao: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+        tipo: "Home Office",
+    },
+    {
+        id: 2,
+        nome: "Pessoa Desenvolvedora Back-End - Node JS",
+        empresa: "Brazilians in Tech",
+        localEmpresa: "Rio de Janeiro",
+        descricao: " Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus blanditiis id deleniti cupiditate at sit officia iure quas culpa laudantium earum in, omnis aut? Expedita provident mollitia consectetur incidunt! Hic.",
+        tipo: "Presencial",
+    },
+    {
+        id: 3,
+        nome: "Pessoa Desenvolvedora Mobile - React Native",
+        empresa: "Brazilians in Tech",
+        localEmpresa: "Rio de Janeiro",
+        descricao: "Lorem ipsum dolor sit amet consectetur adipisicing elit. Commodi voluptatem perspiciatis quam est veniam culpa vero at ad, tempora architecto voluptatibus ducimus debitis illo reiciendis iusto. Deserunt quae quod reiciendis!",
+        tipo: "Híbrido",
+    },
+    {
+        id: 4,
+        nome: "Pessoa Desenvolvedora Mobile - IOS",
+        empresa: "Kenzie Academy",
+        localEmpresa: "Curitiba",
+        descricao: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s",
+        tipo: "Home Office",
+    }
+]
